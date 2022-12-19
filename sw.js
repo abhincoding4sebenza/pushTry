7f82557b1ee2b1962de29d5969d9115f695c421b
abhincoding4sebenza <89969610+abhincoding4sebenza@users.noreply.github.com>
2022-12-19 23:41:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-self.addEventListener('push', (e)=>{
-    var options ={
-        body:" this notification",
-        icon:"./icon.png"
-    }
-    e.waitUntil(self.registration.showNotification('test message',options))
+// self.addEventListener('push', (e)=>{
+//     var options ={
+//         body:" this notification",
+//         icon:"./icon.png"
+//     }
+//     e.waitUntil(self.registration.showNotification('test message',options))
 
-})
+// })
+self.addEventListener('push', () => {
+    self.registration.showNotification('Hello world!', options);
+  });
